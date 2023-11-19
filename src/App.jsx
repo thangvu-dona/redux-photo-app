c186de807b4161a67f74b206b90f74426bf4185e
Thang Vu <thangvu.dona@gmail.com>
2023-11-19 17:08:46 +0700

    add deploy shell script file

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
 import React, { Suspense } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import './App.scss'
 import Header from './components/Header'
 import NotFound from './components/NotFound'
-import Photo from './features/Photo'
 
 // Lazy load - Code Splitting
-// const Photo = React.lazy(() => import('./features/Photo'))
+const Photo = React.lazy(() => import('./features/Photo'))
 
 function App() {
   return (
     <div className="photo-app">
-      {/* <Suspense fallback={<div>Loading ...</div>}> */}
-      <BrowserRouter>
-        <Header />
+      <Suspense fallback={<div>Loading ...</div>}>
+        <BrowserRouter>
+          <Header />
 
-        <Routes>
-          <Route path="/photos/*" element={<Photo />} />
-          <Route element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-      {/* </Suspense> */}
+          <Routes>
+            <Route path="/" element={<h1>Home Page Component</h1>} />
+            <Route path="/photos/*" element={Photo} />
+            <Route element={<NotFound />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
     </div>
   )
 }
